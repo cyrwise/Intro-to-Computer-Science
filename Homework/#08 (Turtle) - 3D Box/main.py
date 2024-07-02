7f2ee3acb37606be41d22cb7f3d6db72e7a9c8de
Cyrus Wise <147113250+cyrwise@users.noreply.github.com>
2024-07-01 21:27:47 -0700

    Create main.py

---
+++ Homework/#08 (Turtle) - 3D Box/main.py
@@ -0,0 +1,41 @@
+# start settings
+import turtle
+turtle.speed(0)
+#hide turtle
+turtle.hideturtle()
+
+# Making the squares
+turtle.setheading(-90)
+turtle.forward(100)
+turtle.setheading(0)
+turtle.forward(100)
+turtle.setheading(90)
+turtle.forward(100)
+turtle.setheading(-180)
+turtle.forward(200)
+turtle.setheading(90)
+turtle.forward(100)
+turtle.setheading(0)
+turtle.forward(100)
+turtle.setheading(-90)
+turtle.forward(100)
+
+# the middle of the square is = to
+# the side * root of 2 = 141.421356237
+turtle.setheading(-45)
+turtle.forward(141.421356237)
+turtle.setheading(135)
+turtle.penup()
+turtle.forward(141.421356237)
+turtle.pendown()
+turtle.forward(141.421356237)
+turtle.penup
+turtle.setheading(-90)
+turtle.forward(100)
+turtle.pendown
+turtle.setheading(-45)
+turtle.forward(141.421356237)
+
+#final side
+turtle.goto(0,100)
+turtle.forward(141.421356237)
