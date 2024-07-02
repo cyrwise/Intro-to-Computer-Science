226f9207fa72e42b2f2ead388fb1a2d05306e975
Cyrus Wise <147113250+cyrwise@users.noreply.github.com>
2024-07-01 21:23:20 -0700

    Create main.py

---
+++ Homework/#04 (Python) - Temperature Conversion/main.py
@@ -0,0 +1,15 @@
+# 2-9 Celsius to Fahrenheit Temp Converter Program
+# 2/28/2021
+# Around 15-20 minutes
+
+# Asking for temperature
+celsius = float(input("Enter the Celsius temperature: "))
+
+# Conversion
+fahrenheit = (9/5)*celsius + 32
+
+# Display produced value
+print("The temperature is equal to", fahrenheit, "fahrenheit.")
+
+# I didn't add extra decimal points or change the spacing because I
+# am not sure if that is asked for. They are fun to use though
