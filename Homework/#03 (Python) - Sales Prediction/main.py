7d546c9878330c2b8e648357d0d100deb8922cd5
Cyrus Wise <147113250+cyrwise@users.noreply.github.com>
2024-07-01 21:22:02 -0700

    Create main.py

---
+++ Homework/#03 (Python) - Sales Prediction/main.py
@@ -0,0 +1,16 @@
+# Sales Prediction P2-2 Program
+# 2/18/2021
+# About 20 minutes for this one
+
+# Declare Real salesTotal and profit
+salesTotal = 0.0
+profit = .23
+
+# Enter the sales number
+salesTotal = float(input("Enter the projected sales: "))
+
+# profit equation
+profit = salesTotal * 0.23
+
+# Display the projected profit
+print("The projected profit is", profit)
